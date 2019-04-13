217a9f20a1f0cf7c5b809e0cf161dc46f9dd9a82
iiclear <43413171+iiclear@users.noreply.github.com>
2019-04-13 10:32:02 +0800

    原始版本

---
+++ test/main.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+import aiml
+import os, sys
+import jieba
+def wordSegment(text):
+    text = text.strip()
+    seg_list = jieba.cut(text)
+    result = " ".join(seg_list)
+    return result
+
+mybot_path = './'
+os.chdir(mybot_path)
+
+mybot = aiml.Kernel()
+mybot.learn('study.aiml')
+while True:
+    input_message = raw_input(">>: ")
+    message = wordSegment(input_message)
+    print message
+    print mybot.respond(message)
+
